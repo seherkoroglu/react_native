81f5aa42eab73875e820a82b1744f455fdcd4abf
seherkoroglu <seherkor2013@gmail.com>
2023-04-28 18:38:17 +0300

    organized

---
+++ pages/programCreation/styles.js
@@ -0,0 +1,32 @@
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    textStyle: {
+      color: '#fff',
+      fontSize: 25,
+      fontWeight: '500',
+      textAlign: 'center',
+     
+    },
+    sonStyle: {
+      color: '#fff',
+      fontSize: 22,
+      fontWeight: '300',
+      textAlign: 'left',
+      paddingBottom: 10,
+     
+    },
+    sonView: {
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      backgroundColor: '#191924',
+      flexDirection: 'column',
+      paddingBottom: 20,
+      
+      
+    },
+  
+  });
+
+    export default styles;
