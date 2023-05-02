a0be05b150d72ed83835433a3d45712720156fdf
seherkoroglu <seherkor2013@gmail.com>
2023-05-02 15:27:59 +0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from '.../pages/firstPage/firstPage';
+import App from './pages/startPage';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
